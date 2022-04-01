c30f82ad38d88dd666229ec9e860fc355be87f45
Jessica Edmonds <jessica1329b@gmail.com>
2022-04-01 15:21:51 -0400

    static exercise

---
+++ Static Exercise/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Static_Exercise
+{
+    public static class TempConverter
+    {
+        public static class TempConverter 
+        {
+            public static double FahrenheitToCelsius(double fahrenheit) 
+            {
+                var result = (fahrenheit - 32) / 1.8;
+
+                return result;  
+            }
+
+            public static double CelciusToFahrenheit(double celcius) 
+            {
+                return (celcius * 9) / 5 + 32;
+            }
+       
+                
+                
+                
+                
+                
+                
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            var celcius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelciusToFahrenheit(20);
+
+
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"celcius {celcius}");
+            Console.WriteLine($"celcius {fahrenheit}");
+
+
+
+        }
+    }
+}
